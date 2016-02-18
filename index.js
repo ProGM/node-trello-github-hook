3178ab719f23cb46bd9906854b82400e361b1ee0
ProGM <progiemmeh@gmail.com>
2016-02-18 11:31:26 +0100

    Added more logging, added license, fixed bug on closed.

--- index.js
+++ index.js
@@ -17,9 +17,8 @@ var t = new Trello(process.env.TRELLO_KEY, process.env.TRELLO_TOKEN);
 
 app.post('/github_hook', function(req, res) {
 
-  console.log(req.params, req.body)
-
   if (req.body.action == 'opened') {
+    console.log("PROCESSING OPENED TRIGGER");
     var issue_name = req.body.issue.title;
     var card_id = issue_name.match(/\[[a-zA-Z0-9]+\]/)[0].replace('[', '').replace(']', '');
     var issue_url = req.body.issue.html_url;
@@ -32,28 +31,45 @@ app.post('/github_hook', function(req, res) {
       }
       // Se esiste una checklist aggiungi l'elemento
       if (data[0] != undefined) {
-        console.log('checklist exists')
         var checklist_id = data[0].id;
-        console.log(checklist_id);
+        console.log("FOUND CHECKLIST WITH ID: " + checklist_id);
 
         t.post('/1/cards/' + card_id + '/checklist/' + checklist_id + '/checkItem', { name: issue_name + " " + issue_url }, function(err, data) {
-          console.log(data)
-          res.send("OK");
+          if (err) {
+            console.log("ERROR ON CREATING CHECKLIST ITEM:");
+            console.log(err);
+            res.status(err.statusCode).send(err.body);
+          } else {
+            console.log("CHECKLIST ITEM CREATED!");
+            console.log(data);
+            res.send("OK");
+          }
         });
       } else {
         // Altrimenti creala!
-        console.log('checklist does not exists')
+        console.log("CHECKLIST NOT FOUND, CREATING...");
         t.post('/1/cards/' + card_id + '/checklists', { name: 'Github Issues:' }, function(err, data) {
-          console.log(data)
           var checklist_id = data.id;
-          t.post('/1/cards/' + card_id + '/checklist/' + checklist_id + '/checkItem', { name: issue_name + " " + issue_url }, function(err, data) {
-            res.send("OK");
-            console.log(data)
-          });
+          if (err) {
+            res.status(err.statusCode).send(err.body);
+          } else {
+            t.post('/1/cards/' + card_id + '/checklist/' + checklist_id + '/checkItem', { name: issue_name + " " + issue_url }, function(err, data) {
+              if (err) {
+                console.log("ERROR ON CREATING CHECKLIST ITEM:");
+                console.log(err);
+                res.status(err.statusCode).send(err.body);
+              } else {
+                console.log("CHECKLIST ITEM CREATED!");
+                console.log(data);
+                res.send("OK");
+              }
+            });
+          }
         });
       }
     });
   } else if (req.body.action == 'closed') {
+    console.log("PROCESSING OPENED TRIGGER");
     var issue_name = req.body.issue.title;
     var card_id = issue_name.match(/\[[a-zA-Z0-9]+\]/)[0].replace('[', '').replace(']', '');
     var issue_url = req.body.issue.html_url;
@@ -63,13 +79,13 @@ app.post('/github_hook', function(req, res) {
     t.get('/1/cards/' + card_id + '/checklists', function(err, data) {
       // Se esiste una checklist aggiungi l'elemento
       if (data[0] != undefined) {
-        console.log('checklist exists')
+        console.log('checklist exists');
         var checklist_id = data[0].id;
         var checkitems = data[0].checkItems;
         console.log(data[0].checkItems)
         var checkitem = null;
         for (var i = 0; i < data[0].checkItems.length; i++) {
-          if (checkitems[i].name == issue_name) {
+          if (checkitems[i].name == (issue_name + " " + issue_url)) {
             checkitem = checkitems[i].id;
             break;
           }
@@ -79,8 +95,15 @@ app.post('/github_hook', function(req, res) {
           return;
         }
         t.put('/1/cards/' + card_id + '/checklist/' + checklist_id + '/checkItem/' + checkitem, { state: 'complete' }, function(err, data) {
-          res.send('OK!');
-          console.log(data)
+          if (err) {
+            console.log("ERROR WHEN SETTING ITEM COMPLETED");
+            console.log(err);
+            res.status(err.statusCode).send(err.body);
+          } else {
+            console.log("CHECKLIST ITEM SET TO COMPLETE STATUS!");
+            console.log(data);
+            res.send("OK");
+          }
         });
       } else {
         res.send('Ignored');
